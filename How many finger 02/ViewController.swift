6311a3d05cb93bf59be2f9d4d1a5564adc7b5a75
D7702_10 <d7702_10@d7702-10ui-imac.local>
2018-03-26 15:48:32 +0900

    Finger Quiz

--- How many finger 02/ViewController.swift
+++ How many finger 02/ViewController.swift
@@ -8,18 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITextFieldDelegate {
+    @IBOutlet weak var input: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //delegate 연결
+        input.delegate=self
+        input.clearButtonMode = UITextFieldViewMode.always
+        input.placeholder = "입력하세요!";
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func guess(_ sender: Any) {
+        //랜덤 숫자 생성
+        let number = arc4random() % 6
+        print(number)
+        let numberString = String(number)
+        if input.text == numberString {
+            resultLabel.text = "빙고!!!"
+        }else {
+            resultLabel.text = "실패!!! Try again !!! \n 숨겨진 숫자는 현재 \(number)"
+        }
+        
+        
     }
-
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //input.resignFirstResponder()
+        view.endEditing(true)
+        input.backgroundColor = UIColor.blue
+        
+    }
+    
+    func  textFieldShouldReturn(_ textField: UITextField) ->
+        Bool {
+           
+            print("I pressed return key")
+            //버튼을 실행시키면 키보드를내림
+       input.resignFirstResponder()
+            view.backgroundColor = UIColor.yellow
+            return true
+    }
+    
 }
 
